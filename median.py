beb702c323b008aa99d8048c10b8322e55c659fa
JoleneLim22019027 <k22019027@kcl.ac.uk>
2023-10-02 14:59:43 +0100

    first commit

--- median.py
+++ median.py
@@ -1,12 +1,20 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+        numbers = sorted([float(value) for value in input().split(",")])
+        qty = len(numbers)
+        if qty % 2 == 0:
+            x = int(qty/2)
+        else:
+            x = math.ceil((qty/2) - 1)
+        median = numbers[x]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+print(median)
